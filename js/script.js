5df21cc89b20ff43826741d0920ebfb4a7b21e0c
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-08-28 14:14:18 +0100

    Added js function to scroll page to top

--- js/script.js
+++ js/script.js
@@ -35,6 +35,13 @@ allLink.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
+
+    //Scrolling to Top
+    if (href === #)
+      window.scrollTo ({
+        top: 0,
+        behavior: "smooth"
+      })
   });
 });
 //////////////////////////////////////////////////////////
