678494cc7ac574d28aa55c52c381ce07dff32319
ElozinoLopez <asaijeelozinolopez@gmail.com>
2025-08-29 10:05:06 +0100

    Fixed a bug with the smooth scrolling functionality

--- js/script.js
+++ js/script.js
@@ -29,20 +29,20 @@ btnNavEl.addEventListener("click", function () {
 ///////////////////////////////////////////////////////
 //Smmoth scrolling animation
 
-const allLiks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll("a:link");
 
-allLink.forEach(function (link) {
-  link.addEventListener("click", function(e) {
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
 
     //Scrolling to Top
-    if (href === #)
+    if (href === "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
-      })
-    
+      });
+
     // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
@@ -55,27 +55,20 @@ allLink.forEach(function (link) {
   });
 });
 
-
 //////////////////////////////////////////////////////////
 // Sticky navigation
 const sectionHeroEl = document.querySelector(".section-hero");
 
-const obs = new IntersectionObserver (function(entries){
-const ent = entries[0];
-}, 
-{
-  root: null,
-  threshold: 0
-});
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
 obs.observe(sectionHeroEll);
-
-
-
-
-
-
-
-
 
 //////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
